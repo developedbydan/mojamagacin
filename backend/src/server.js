b7446f4b2eb3081a8f11b8f819fc06a8477d816d
Danijel <developedbydane@gmail.com>
2024-08-14 19:37:08 +0200

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
-// Učitaj .env fajl iz trenutnog direktorijuma
 dotenv.config({ path: `src/.env` });
 
 const app = express();
 const port = process.env.PORT;
 
-// // Middleware
-app.use(express.json()); // Omogućava parsiranje JSON tela zahteva
+app.use(express.json());
 
-// // Pokretanje servera
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
