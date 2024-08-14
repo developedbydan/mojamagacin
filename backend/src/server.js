489a975199d09c40f831ddf285d49aaa8f91f95b
Danijel <developedbydane@gmail.com>
2024-08-14 20:09:38 +0200

    Mongo connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({ path: `src/.env` });
 
 const app = express();
 const port = process.env.PORT;
+const mongoDBURI = process.env.MONGO_URI;
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose
+  .connect(mongoDBURI)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(port, () => {
+      console.log(`App is listening to port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
