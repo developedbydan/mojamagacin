22427ef55d8f1711a65fda7e4143283f89f0cc37
Danijel <developedbydane@gmail.com>
2024-08-21 22:03:32 +0200

    feat(product): add update and delete

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -35,11 +35,65 @@ export const createProduct = async (req, res) => {
   }
 };
 
+// Dohvatanje svih proizvoda
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (err) {
-    res.status(500).json({ message: "Greška pri odhvatanju proizvoda." });
+    res.status(500).json({ message: "Greška pri odhvatanju proizvoda.", err });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, category, quantity, price, supplier } = req.body;
+    const userId = req.user._id;
+
+    // Provera da li proizvod postoji i pripada trenutnom korisniku
+    const existingProduct = await Product.findOne({ _id: id, user: userId });
+
+    if (!existingProduct) {
+      return res.status(400).json({ message: "Proizvod nije pronađen." });
+    }
+
+    // Provera kategorije ako se azurira
+    const categoryDoc = await Category.findOne({ _id: category, user: userId });
+    if (!categoryDoc) {
+      return res.status(400).json({ message: "Kategorija nije validna." });
+    }
+
+    existingProduct.name = name || existingProduct.name;
+    existingProduct.category = category || existingProduct.category;
+    existingProduct.quantity = quantity || existingProduct.quantity;
+    existingProduct.price = price || existingProduct.price;
+    existingProduct.supplier = supplier || existingProduct.supplier;
+
+    const updatedProduct = await existingProduct.save();
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json({ message: "Greška pri ažuriranju proizvoda.", err });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user._id;
+
+    const deletedProduct = await Product.findOneAndDelete({
+      _id: id,
+      user: userId,
+    });
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Proizvod nije pronađen." });
+    }
+
+    res.status(200).json({ message: "Proizvod je uspešno obrisan." });
+  } catch (err) {
+    res.status(500).json({ message: "Greška pri brisanju proizvoda.", err });
   }
 };
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
+  updateProduct,
 } from "../controllers/productController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
@@ -9,5 +11,7 @@ const router = express.Router();
 
 router.get("/", authMiddleware, getAllProducts);
 router.post("/", authMiddleware, createProduct);
+router.put("/:id", authMiddleware, updateProduct);
+router.delete("/:id", authMiddleware, deleteProduct);
 
 export default router;
