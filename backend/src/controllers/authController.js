f4c3635a118cdeac266231049acf6976b07793e2
Danijel <developedbydane@gmail.com>
2024-09-13 14:57:11 +0200

    (fix): auth fix

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,13 +6,6 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve("src/.env") });
 
-const jwtSecret = process.env.JWT_SECRET;
-
-if (!jwtSecret) {
-  console.error("JWT_SECRET nije definisan u .env fajlu");
-  process.exit(1);
-}
-
 export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -64,31 +57,79 @@ export const loginUser = async (req, res) => {
       return res.status(400).json({ message: "Pogrešan email ili lozinka" });
     }
 
-    const token = jwt.sign({ _id: user._id }, jwtSecret, { expiresIn: "1h" });
+    const payload = { id: user._id };
 
-    // Postavljanje tokena u HTTP-only cookie
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 3600000, // 1 sat
+    // Generiši access i refresh token
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "15m",
+    });
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "7d",
     });
 
-    res.status(200).json({ message: "Uspešna prijava" });
+    // Postavi access token u HTTP-only kolacic
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000, // 15 minuta
+    });
+
+    // Postavi refresh token u HTTP-only kolacic
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dana
+    });
+
+    res.status(200).json({ username: user.username });
   } catch (err) {
     res.status(500).json({ message: "Prijava nije uspešna", err });
   }
 };
 
-export const logoutUser = async (req, res) => {
-  try {
-    res.clearCookie("token", {
+export const logoutUser = (req, res) => {
+  res.clearCookie("access_token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  });
+
+  res.clearCookie("refresh_token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  });
+
+  res.status(200).json({ message: "Uspešna odjava" });
+};
+
+export const refreshToken = (req, res) => {
+  const refreshToken = req.cookies.refresh_token;
+
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Refresh token nije dostavljen." });
+  }
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: "Nevažeći refresh token" });
+    }
+
+    // Generisi novi access token
+    const newAccessToken = jwt.sign(
+      { id: payload.id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.cookie("access_token", newAccessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000, // 15 minuta
     });
-    res.status(200).json({ message: "Uspešna odjava" });
-  } catch (err) {
-    res.status(500).json({ message: "Odjava nije uspešna.", err });
-  }
+
+    res.status(200).json({ message: "Access token obnovljen." });
+  });
 };
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../api/auth.js";
 import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 
-const Login = ({ setIsLoggedIn }) => {
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -17,12 +18,6 @@ const Login = ({ setIsLoggedIn }) => {
 
     try {
       await login(email, password);
-      console.log("Ulogovan");
-
-      // Azuriranje stanja u App
-      setIsLoggedIn(true);
-
-      // Preusmeravanje na dashboard
       navigate("/");
     } catch (err) {
       if (err.response && err.response.data) {
@@ -34,6 +29,7 @@ const Login = ({ setIsLoggedIn }) => {
       }
     }
   };
+
   return (
     <div className="flex flex-col  items-center w-full h-lvh bg-slate-950">
       <div className=" flex flex-col  items-center w-68  mt-40 bg-slate-900 p-10 rounded-xl">
